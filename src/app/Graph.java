47d416b33c5b73af233d8c6859b14aba47d88356
Vitalii <vitaliti.yurchenko@gmail.com>
2024-12-14 00:29:21 +0200

    Methods for working with the graph are encapsulated

--- src/app/Graph.java
+++ src/app/Graph.java
@@ -13,20 +13,20 @@ public class Graph {
         adjacencyList = new HashMap<>();
     }
 
-    public void addVertex(int vertex) {
+    private void addVertex(int vertex) {
         if (!adjacencyList.containsKey(vertex)) {
             adjacencyList.put(vertex, new ArrayList<>());
         }
     }
 
-    public void addEdge(int source, int destination) {
+    private void addEdge(int source, int destination) {
         addVertex(source);
         addVertex(destination);
         adjacencyList.get(source).add(destination);
         adjacencyList.get(destination).add(source);
     }
 
-    public void removeVertex(int vertex) {
+    private void removeVertex(int vertex) {
         if (adjacencyList.containsKey(vertex)) {
             for (int neighbor : adjacencyList.get(vertex)) {
                 adjacencyList.get(neighbor).remove(Integer.valueOf(vertex));
@@ -35,7 +35,7 @@ public class Graph {
         }
     }
 
-    public void removeEdge(int source, int destination) {
+    private void removeEdge(int source, int destination) {
         if (adjacencyList.containsKey(source)) {
             adjacencyList.get(source).remove(Integer.valueOf(destination));
         }
@@ -44,17 +44,42 @@ public class Graph {
         }
     }
 
-    public boolean hasVertex(int vertex) {
+    private boolean hasVertex(int vertex) {
         return adjacencyList.containsKey(vertex);
     }
 
-    public boolean hasEdge(int source, int destination) {
+    private boolean hasEdge(int source, int destination) {
         return adjacencyList.containsKey(source) && adjacencyList.get(source).contains(destination);
     }
 
-    public void printGraph() {
+    private void printGraph() {
         for (Map.Entry<Integer, List<Integer>> entry : adjacencyList.entrySet()) {
             System.out.println("Vertex " + entry.getKey() + ": " + entry.getValue());
         }
+    }
+
+    // Методи-обгортки для доступу до приватних методів
+    public void availableAddVertex(int vertex) {
+        addVertex(vertex);
+    }
+
+    public void availableAddEdge(int source, int destination) {
+        addEdge(source, destination);
+    }
+
+    public void availableRemoveVertex(int vertex) {
+        removeVertex(vertex);
+    }
+
+    public void availableRemoveEdge(int source, int destination) {
+        removeEdge(source, destination);
+    }
+
+    public boolean availableHasVertex(int vertex) {
+        return hasVertex(vertex);
+    }
+
+    public boolean availableHasEdge(int source, int destination) {
+        return hasEdge(source, destination);
     }
 }
